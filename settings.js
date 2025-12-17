83da925a0eb0e6f69d312f9281f3722ae0ded731
codingmaster <153460070+starlightGG@users.noreply.github.com>
2025-12-16 20:04:17 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -212,6 +212,7 @@ setTimeout(() => {
         } else {
             aboutBlankToggleSwitch.classList.remove('switch-on');
             if(aboutBlankSwitchIcon) aboutBlankSwitchIcon.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />`;
+            showModal("I wouldn't recommend turning this option off since 'GoGuardian' can see your tabs and this can popup in history, if you want, you can enable it back.");
         }
     }
 
