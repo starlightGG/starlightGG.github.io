d9297eed340b51d1b7773da877f74c28a5e5ffaa
codingmaster <153460070+starlightGG@users.noreply.github.com>
2026-01-14 10:23:57 -0600

    Update cloner.js

--- js/cloner.js
+++ js/cloner.js
@@ -1,3 +1,4 @@
+
 // js/cloner.js
 
 // 1. Tip Advisory (Defined but needs to be called if you want it to run)
@@ -15,7 +16,7 @@ function executeCloningLogic() {
     const savedState = localStorage.getItem('aboutBlankPopupState');
 
     // Only attempt cloning if enabled and not already in about:blank
-    if (window.top.location.href !== 'about:blank' && savedState === 'true') {
+    if (window.location.href !== 'about:blank' && savedState === 'true') {
         
         // Attempt to open new window
         const newWindow = window.open('about:blank', '_blank');
@@ -42,71 +43,26 @@ function executeCloningLogic() {
             window.location.replace(savedLink ? savedLink : 'https://google.com/');
             
         } else {
-
-        // --- FAILURE (Popup Blocked) ---
+            // --- FAILURE (Popup Blocked) ---
             
+            // Instead of calling showModal() which doesn't exist here,
+            // we update the CloakingAlert text to warn the user.
             if (cloakElement) {
                 const textDiv = cloakElement.querySelector('div:last-child');
-                const Loader = cloakElement.querySelector('div:first-child');
-                
-                // Remove the loader since it failed
-                if (Loader) Loader.remove();
-
                 if (textDiv) {
-                    // Update text to tell them clicking will try again
-                    textDiv.innerText = "Popups Blocked! Click here to open the window manually.";
-                    textDiv.style.color = "#ff4444"; 
+                    textDiv.innerText = "Popups Blocked! Please Enable Popups in Order to Cloak Window. \n(Click to dismiss)";
+                    textDiv.style.color = "#ff4444"; // Red color for error
                 }
                 
+                // Allow user to click to dismiss if it failed
                 cloakElement.style.cursor = "pointer";
-                cloakElement.title = "Click to open window and dismiss";
-
-                // --- NEW ONCLICK LOGIC ---
+                cloakElement.title = "Click to close";
                 cloakElement.onclick = function(e) {
-// Prevent default browser behavior (stops page jumps/reloads)
-                    if (e) e.preventDefault(); 
-                    // Stop the click from bubbling up to parents (optional but good for modals)
-                    if (e) e.stopPropagation();
-                    // 1. Try to open the popup AGAIN. 
-    const cloakElement2 = document.getElementById('CloakingAlert');
-    const savedState2 = localStorage.getItem('aboutBlankPopupState');
-
-        
-        // Attempt to open new window
-        const newWindow2 = window.open('about:blank', '_blank');
-
-        if (newWindow2) {
-            // --- SUCCESS ---
-            
-            // 1. Remove this script so it doesn't loop in the new tab
-            const scriptElement2 = document.getElementById('cloning-script');
-            if (scriptElement2) scriptElement2.remove();
-            
-            // 2. Remove the cloak from the DOM to be copied (so the new tab doesn't start cloaked)
-            if (cloakElement) cloakElement2.remove();
-            
-            // 3. Clone HTML
-            const currentHtml2 = document.documentElement.outerHTML;
-
-            // 4. Write to new window
-            newWindow2.document.write(currentHtml2);
-            newWindow2.document.close();
-
-            // 5. Redirect original tab
-            const savedLink2 = localStorage.getItem('LINKTAB_KEY');
-            window.location.replace(savedLink2 ? savedLink2 : 'https://google.com/');
-            
-
-                    // 2. Run the fade out animation (happens regardless of success/fail)
+                    // Fade out effect
                     this.style.transition = "opacity 0.5s ease";
                     this.style.opacity = "0";
-
-                    // 3. Destroy the element after animation
-                    setTimeout(() => {
-                        this.remove();
-                    }, 500);
+                    setTimeout(() => { this.remove(); }, 500);
                 };
-            
             }
         }
     } else {
